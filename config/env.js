319c488aaf801838fe0e06775a3978039ee815bc
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-06-21 23:32:49 +0530

    Added Mongo-connectio-url

--- config/env.js
+++ config/env.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
-  MONGO_CONNECTION_URL: process.env.MONGO_CONNECTION_URL,
+  MONGO_CONNECTION_URL: process.env.MONGO_CONNECTION_URL || "mongodb://127.0.0.1:27017",
   SIZE: process.env.SIZE || 1000000 * 100,
   PORT: process.env.PORT || 3010,
 };
