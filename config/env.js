736e1016273bc6ed1ef0f6623cc35e44a965179d
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-06-21 23:22:21 +0530

    Added size

--- config/env.js
+++ config/env.js
@@ -10,6 +10,6 @@ dotenv.config({
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   MONGO_CONNECTION_URL: process.env.MONGO_CONNECTION_URL,
-  SIZE: process.env.SIZE,
+  SIZE: process.env.SIZE || 1000000 * 100,
   PORT: process.env.PORT || 3010,
 };
