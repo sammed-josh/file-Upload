dd9ae3f1b12beed7430687e7cd3447e54d3da150
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-06-23 14:06:17 +0530

    Added register model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", user);
