03ba07897aca1f54af60687b66b9b05485a40705
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-06-24 15:02:04 +0530

    Added JWT auth for File-upload

--- routes/userFile.js
+++ routes/userFile.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 const { fileUploader } = require("../controller/userController");
 const upload = require("../middleware/fileUploader");
+const auth = require("../utils/jwt")
 
-router.post("/", upload, fileUploader);
+router.post("/upload",auth, upload, fileUploader);
 
 module.exports = router;
