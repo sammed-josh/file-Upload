5bbd4043ca2c473bd79726ec6c362e44e90ee84c
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-06-28 11:19:49 +0530

    Integrating Socket.io

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const connectDB = require("./utils/db");
 
 const app = express();
 
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
 const logger = require("./logger/logger");
 
 app.use("/files", require("./routes/userFile"));
@@ -15,6 +22,10 @@ app.get("/", (req, res) => {
 
 connectDB().catch((error) => {
   logger.error(error);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 app.listen(PORT, () => {
